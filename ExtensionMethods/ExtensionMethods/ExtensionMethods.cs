8d4013caf5860755426f94ee3fc5155c0a7ae649
MariusA <anca.ticau@gmail.com>
2019-07-04 23:52:48 +0300

    innitial commit-ExtensionMethods

---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    public static class ExtensionMethods
+    {
+        public static string CapitalizeFirstLetter(this string inputString)
+        {
+            if (string.IsNullOrWhiteSpace(inputString))
+            {
+                return inputString;
+            }
+
+            string firstLetter = (char.IsUpper(inputString[0]) ? inputString[0].ToString() : char.ToUpper(inputString[0]).ToString());
+
+            return firstLetter + inputString.Substring(1);
+        }
+
+        public static string Pluralize(this string inputString)
+        {
+            if (string.IsNullOrWhiteSpace(inputString))
+            {
+                return inputString;
+            }
+
+            return inputString + "s";
+        }
+
+        public static void CheckIfFirstLetterIsCapital(this string inputString)
+        {
+            if (inputString[0] == char.ToUpper(inputString[0]))
+            {
+                Console.WriteLine("The string starts with capital letter");
+            }
+            else
+            {
+                Console.WriteLine("The string does not start with capital letter");
+            }
+        }
+
+        public static int CountChar(this string inputString)
+        {
+            return inputString.Count();
+        }
+
+        public static string CapitalizeAndPluralize(this string inputString)
+        {
+            return inputString.CapitalizeFirstLetter().Pluralize();
+        }
+
+        public static decimal ApplyVat(this decimal inputDecimal)
+        {
+            if (inputDecimal <= 0)
+            {
+                return inputDecimal;
+            }
+            return inputDecimal * 1.24M;
+        }
+    }
+}
+
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Capitalize word
+            // Write an extension method that will capitalize a word.
+            // Input: mom
+            // Output: Mom
+
+            string parent = "mom";
+
+            string result = parent.CapitalizeFirstLetter();
+
+            Console.WriteLine(result);
+
+
+            //Pluralize
+            //    Write an extension method that appends an ‘s’ to a string.
+            //    Ex:
+            //input: method
+            //output:methods
+
+            string givenString = "method";
+
+            string result1 = givenString.Pluralize();
+
+            Console.WriteLine(result1);
+
+            //Capital letter
+            //Write an extension method to check if a string starts with a capital letter or not.
+
+            givenString = "This is a string";
+            givenString.CheckIfFirstLetterIsCapital();
+
+            //Character count
+            //Write an extension method what will count the number of characters in a string.
+
+            string givenString1 = "abc";
+
+            int charCount = givenString1.CountChar();
+
+            Console.WriteLine($"Number of characters in '{givenString1}' is '{charCount}'.");
+
+
+            // Capitalize and pluralize
+            // Write a program that will capitalize a string, and pluralize the same string.
+            // You cand write a new extension method or use what you already have.
+
+            string givenString2 = "house";
+
+            Console.WriteLine(givenString2.CapitalizeAndPluralize());
+
+            //Apply VAT
+            //Write an extension method that will apply a VAT of 24 % on an input. This input can be a decimal value, or an integer on
+
+            decimal givenValue = 100M;
+
+            Console.WriteLine(givenValue.ApplyVat());
+        }
+    }
+}
+
